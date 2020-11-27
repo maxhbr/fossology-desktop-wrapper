44567d5ec2a92712187b28e9f698cc62a611953f
Maximilian Huber <gh@maxhbr.de>
2020-11-27 18:54:38 +0100

    initial commit of scripts
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+# SPDX-FileCopyrightText: 2020 Maximilian Huber <gh@maximilian-huber.de>
+#
+# SPDX-License-Identifier: GPL-2.0-only
+
 set -ex
 cd "$(dirname "$0")"
 docker-compose start
